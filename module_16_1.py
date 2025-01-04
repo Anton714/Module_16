2152208c64907bac2a3e1ff431c9065221feb60f
Anton714 <an714.am@gmail.com>
2025-01-04 12:17:11 +0500

    Add files via upload

---
+++ module_16_1.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def welcome():
+    return f"Главная страница"
+
+
+@app.get("/user/admin")
+async def admin () :
+    return f"Вы вошли как администратор"
+
+
+@app.get("/user/{user_id}")
+async def user_in(user_id: int):
+    return f"Вы вошли как пользователь № {user_id}"
+
+@app.get("/user")
+async def obj (username: str, age: int):
+    return f"Информация о пользователе. Имя: {username}, Возраст:{age}"
